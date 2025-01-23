58cf7dc970caf74a65693e0f177b947c4e4f034e
Jason Xu <jason.xu@bjss.com>
2025-01-23 14:44:19 +0000

    added mulock for in memory store

--- inMemStorage.go
+++ inMemStorage.go
@@ -1,14 +1,23 @@
 package main
 
+import "sync"
+
 type InMemoryStorage struct {
 	todos []Todo
+	mu    sync.Mutex
 }
 
 func (s *InMemoryStorage) Save(todos []Todo) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	s.todos = todos
 	return nil
 }
 
 func (s *InMemoryStorage) Load() ([]Todo, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	return s.todos, nil
 }
